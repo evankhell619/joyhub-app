39f8d6f46bebc812b1c2716238ceeb47ad81186c
Evan Morningstar <evankhell619@gmail.com>
2024-05-05 16:42:51 +0700

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
+import Navbar from "./components/Navbar/Navbar";
+import {
+  Actors,
+  MovieInformation,
+  Movies,
+  Profile,
+} from "./components/PageIndex";
+import useStyle from "./style";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1>Movies</h1>,
-  },
-  {
-    path: "/movie/:id",
-    element: <h1>Movie Information</h1>,
-  },
-  {
-    path: "/actors/:id",
-    element: <h1>Actor</h1>,
-  },
-  {
-    path: "/profile/:id",
-    element: <h1>Profile</h1>,
-  },
-]);
+const theme = createTheme({});
+const App = () => {
+  const classes = useStyle();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <CssBaseline />
-    </RouterProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
