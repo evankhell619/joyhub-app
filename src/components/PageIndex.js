248b96e7983d58ec2f531a191290691ae8672619
Evan Morningstar <evankhell619@gmail.com>
2024-05-07 21:28:05 +0700

    add render data movie

--- src/components/PageIndex.js
+++ src/components/PageIndex.js
@@ -3,3 +3,5 @@ export { default as Movies } from "./Movies/Movies.jsx";
 export { default as MovieInformation } from "./MovieInformation/MovieInformation.jsx";
 export { default as Profile } from "./Profile/Profile.jsx";
 export { default as Sidebar } from "./Sidebar/Sidebar.jsx";
+export { default as MovieList } from "./MovieList/MovieList.jsx";
+export { default as Movie } from "./Movie/Movie.jsx";
